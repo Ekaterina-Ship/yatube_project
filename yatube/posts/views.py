6a37754ad4b20e75fc371b9a72d4814125b311ec
Екатерин Шипулина <eva_vs@mail.ru>
2022-09-25 00:34:55 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
-def group_posts(request, slug):
-    return render(request, slug)
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_posts(request, any_slug):
+    return HttpResponse(f'Запись постов {any_slug}')
